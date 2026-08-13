61a66bcb2ded4496f2b4caf4aa4fd042a8c00426
Arun Kumar Yadav <aruny5850@gmail.com>
2026-08-13 23:34:00 +0530

    Update README.py

--- README.py
+++ README.py
@@ -148,11 +148,7 @@ button[data-baseweb="tab"] {
         margin-bottom:12px;
     }
 [data-testid="stHorizontalBlock"] {
-    gap: 0.15rem !important;
-}
-
-[data-testid="stVerticalBlock"] {
-    gap: 0.15rem !important;
+    gap: 0.5rem !important;
 }
 }
     .hero h1 {
